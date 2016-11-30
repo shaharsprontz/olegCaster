167c9701ed77d97cf4ebd7f9c3d7fa62f54a281f
shaharsprontz <shahar@getstocks.com>
2016-11-30 15:12:32 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,15 +1,21 @@
 function sentenceGenerator(){
 var stories = ["All puzzles are missing a square",
-               "yes yes",
+               "Yes Yes",
                "poopUp",
                "First Cless",
                "Why the voice in the elevator isn't sexy",
-               "If you're not walking forward, you are walking backwords"], 
-    i = -1;
+               "If you're not walking forward, you are walking backwords",
+               "Why Humus has tahini in it?!", "Third time, Heartburn", "A black elephant passed through them", "Sometimes there are waves and sometimes traffic",
+               "I ate peanuts on suturday", "Happy half day!", "Life is like cheese", ""], 
+    
+i = -1;
 (function f(){
     i = (i + 1) % stories.length;
     var div = document.getElementById('displayLines');
+    //div.style.color = "#2ECC71";
     div.innerHTML = stories[i];
-    setTimeout(f, 5000);
+    $("#displayLines").fadeIn(3000);
+    setTimeout(f, 10000);
+    $("#displayLines").fadeOut(3000);
  })();
 };
