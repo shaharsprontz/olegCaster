eba4022856e56fcbeeb36b0ff175b041f3a41934
shaharsprontz <sh.sprontz@gmail.com>
2016-11-30 21:57:59 +0200

    Adding a media folder and some videos

--- source.js
+++ source.js
@@ -1,4 +1,4 @@
-function sentenceGenerator(){
+function sentenceGenerator() {
 var stories = ["לכל פאזל חסרה קובייה",
                "כן כן",
                "poopUp",
@@ -6,8 +6,8 @@ var stories = ["לכל פאזל חסרה קובייה",
                "למה אין קול סקסי במעלית",
                "מי שהולך קדימה הולך אחורה",
                "למה חומוס זה עם טחינה", "פעם שלישית צרבת", "עבר ביניהם פיל שחור", "לפעמים יש גלים ויש פקקים",
-               "אכלתי בוטנים בשבת", "חצי יום שמח", "החיים זה כמו גבינה"], 
-    
+               "אכלתי בוטנים בשבת", "חצי יום שמח", "החיים זה כמו גבינה"],
+
 i = -1;
 (function f(){
     i = (i + 1) % stories.length;
@@ -18,4 +18,39 @@ i = -1;
     setTimeout(f, 10000);
     $("#displayLines").fadeOut(3000);
  })();
-};
+//
+// //
+// // var videos = [
+// //     'IMG_6222.MOV',
+// //     'IMG_6223.MOV',
+// //     'IMG_6224.MOV'
+// // ];
+// //
+// // var index=Math.floor(Math.random() * videos.length);
+// // var html='<div id="vid" <iframe width="100%" height="200" src="https://github.com/shaharsprontz/olegCaster/tree/gh-pages/media' + videos[index] + '" frameborder="0" allowfullscreen></iframe></div>';
+// // document.write(html)
+// }
+     
+     
+     function rand(n) {
+         return ( Math.floor(Math.random() * n + 1) );
+     }
+
+// Store youtube [CHANGES NEEDED IN THE 3 URLS BELOW]
+     var vids = [];
+     vids[0] = "https://github.com/shaharsprontz/olegCaster/blob/gh-pages/media/IMG_6222.MOV";
+     vids[1] = "https://github.com/shaharsprontz/olegCaster/blob/gh-pages/media/IMG_6223.MOV";
+     vids[2] = "https://github.com/shaharsprontz/olegCaster/blob/gh-pages/media/IMG_6224.MOV";
+
+
+// Pick a random video from the list
+     function pick_vid() {
+         var numberOfImages = 3; //[CHANGE THE 3 TO THE TOTAL NUMBER OF VIDS YOU USE]
+         var num = rand(numberOfImages) - 1;
+
+         document.getElementById("random_youtube_vid").src = vids[num];
+     }
+
+     window.onload = pick_vid;
+
+ }
