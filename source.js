3788ec5416b45dabb323b3490a198720ab971d8f
shaharsprontz <shahar@getstocks.com>
2016-12-13 18:27:46 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,12 +1,12 @@
 function sentenceGenerator() {
-var stories = ["לכל פאזל חסרה קובייה",
-               "כן כן",
-               "poopUp",
-               "First Cless",
-               "למה אין קול סקסי במעלית",
-               "מי שהולך קדימה הולך אחורה",
-               "למה חומוס זה עם טחינה", "פעם שלישית צרבת", "עבר ביניהם פיל שחור", "לפעמים יש גלים ויש פקקים",
-               "אכלתי בוטנים בשבת", "חצי יום שמח", "החיים זה כמו גבינה"],
+// var stories = ["לכל פאזל חסרה קובייה",
+//                "כן כן",
+//                "poopUp",
+//                "First Cless",
+//                "למה אין קול סקסי במעלית",
+//                "מי שהולך קדימה הולך אחורה",
+//                "למה חומוס זה עם טחינה", "פעם שלישית צרבת", "עבר ביניהם פיל שחור", "לפעמים יש גלים ויש פקקים",
+//                "אכלתי בוטנים בשבת", "חצי יום שמח", "החיים זה כמו גבינה"],
 
 i = -1;
 (function f(){
@@ -21,27 +21,27 @@ i = -1;
 };
 
 
-//     function rand(n) {
-//         return ( Math.floor(Math.random() * n + 1) );
-//     }
+    function rand(n) {
+        return ( Math.floor(Math.random() * n + 1) );
+    }
 
-// // Store youtube [CHANGES NEEDED IN THE 3 URLS BELOW]
-//     var vids = [];
-//     vids[0] = "https://youtu.be/dopR7uhKErQ?autoplay=1";
-//     vids[1] = "https://youtu.be/D3NxCJNMHNM?autoplay=1";
-//     vids[2] = "https://youtu.be/OvJtEIHUOis?autoplay=1";
+// Store youtube [CHANGES NEEDED IN THE 3 URLS BELOW]
+    var vids = [];
+    vids[0] = "https://youtu.be/dopR7uhKErQ?autoplay=1";
+    vids[1] = "https://youtu.be/D3NxCJNMHNM?autoplay=1";
+    vids[2] = "https://youtu.be/OvJtEIHUOis?autoplay=1";
 
 
-// // Pick a random video from the list
-//     function pick_vid() {
-//         var numberOfImages = 3; //[CHANGE THE 3 TO THE TOTAL NUMBER OF VIDS YOU USE]
-//         var num = rand(numberOfImages) - 1;
+// Pick a random video from the list
+    function pick_vid() {
+        var numberOfImages = 3; //[CHANGE THE 3 TO THE TOTAL NUMBER OF VIDS YOU USE]
+        var num = rand(numberOfImages) - 1;
 
-//         document.getElementById("random_youtube_vid").src = vids[num];
-//     }
+        document.getElementById("random_youtube_vid").src = vids[num];
+    }
 
-//     window.onload = pick_vid;
-//     setTimeout(function(){
-//     location.reload();
-//     }, 10000);
+    window.onload = pick_vid;
+    setTimeout(function(){
+    location.reload();
+    }, 10000);
 
