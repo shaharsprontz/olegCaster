57bb9b60f80d13e1d9ec4aa8cddb4fdf898c9fb5
shaharsprontz <shahar@getstocks.com>
2016-12-13 18:29:07 +0200

    Update source.js

--- source.js
+++ source.js
@@ -8,17 +8,17 @@ function sentenceGenerator() {
 //                "למה חומוס זה עם טחינה", "פעם שלישית צרבת", "עבר ביניהם פיל שחור", "לפעמים יש גלים ויש פקקים",
 //                "אכלתי בוטנים בשבת", "חצי יום שמח", "החיים זה כמו גבינה"],
 
-i = -1;
-(function f(){
-    i = (i + 1) % stories.length;
-    var div = document.getElementById('displayLines');
-    //div.style.color = "#2ECC71";
-    div.innerHTML = stories[i];
-    $("#displayLines").fadeIn(3000);
-    setTimeout(f, 10000);
-    $("#displayLines").fadeOut(3000);
- })();
-};
+// i = -1;
+// (function f(){
+//     i = (i + 1) % stories.length;
+//     var div = document.getElementById('displayLines');
+//     //div.style.color = "#2ECC71";
+//     div.innerHTML = stories[i];
+//     $("#displayLines").fadeIn(3000);
+//     setTimeout(f, 10000);
+//     $("#displayLines").fadeOut(3000);
+//  })();
+// };
 
 
     function rand(n) {
